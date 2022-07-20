5625ae96b34571fdc9f755d73b83795f5daa42a5
EasyHoons <97458013+EasyHoons@users.noreply.github.com>
2022-07-20 12:04:57 +0900

    initial package design

---
+++ SpringWeb-Board/src/main/java/com/ex/board/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.ex.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String root(){
+		return "redirect:/index";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/index")
+	public String index() {
+		return "hghi";
+	}
+	
+
+}

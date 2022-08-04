f0075ab6db716fe6e6332ef16b18f467987718dc
EasyHoons <97458013+EasyHoons@users.noreply.github.com>
2022-08-04 14:49:14 +0900

    test ver

--- SpringWeb-Board/src/test/java/com/ex/board/SpringWebBoardApplicationTests.java
+++ SpringWeb-Board/src/test/java/com/ex/board/SpringWebBoardApplicationTests.java
@@ -27,16 +27,29 @@ class SpringWebBoardApplicationTests {
 
 	    @Test
 	    void testJpa() {
+	    	
+	    	
 //	        for (int i = 1; i <= 50; i++) {
 //	            String subject = String.format("테스트 데이터입니다:[%03d]", i);
 //	            String content = "페이징 테스트 데이터입니다.";
 //	            this.messageService.create(subject, content,null);
 //				}
-	    	List<Message> all = this.messagerepository.findAll();
-	    	SiteUser siteuser=this.userrepository.getById((long) 4); //id로 유저 호출
-	    	System.out.println(all.size());    	
-	    	System.out.println(this.messagerepository.count());
-	    	long test = this.messagerepository.countByAuthor(siteuser); //호출한 유저가 쓴 글수
-	    	System.out.println(test);
+	    	
+	    	//test JPA
+//	    	List<Message> all = this.messagerepository.findAll();
+//	    	SiteUser siteuser=this.userrepository.getById((long) 4); //id로 유저 호출
+//	    	System.out.println(all.size());    	
+//	    	System.out.println(this.messagerepository.count());
+//	    	long test = this.messagerepository.countByAuthor(siteuser); //호출한 유저가 쓴 글수
+//	    	System.out.println(test);
+	    	
+	    	
+//	    	SiteUser siteuser=this.userrepository.getById((long) 4);
+//	    	System.out.println(this.messagerepository.countAuthor(siteuser));
+	    	List<Message> all = this.messagerepository.selectAllJPQL();
+	    	System.out.println(all.get(1));
+	    	System.out.println(this.messagerepository.justSubject().get(1).toString());
+	    	System.out.println(this.messagerepository.count(4));
+
 	    }
 }
